55258cae8981b3364380dbd8aedf945484065855
Evgeniy-Om <evgenicuss@gmail.com>
2022-03-14 16:22:35 +0300

    настройка поиска фильмов по фильтрам

--- src/modules/film/film.controller.ts
+++ src/modules/film/film.controller.ts
@@ -1,7 +1,6 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query, UseGuards } from '@nestjs/common'
+import { Body, Controller, Get, Post, Query, UseGuards } from '@nestjs/common'
 import { FilmService } from './film.service'
 import { AddFilmToListDto } from './dto/add-film-to-list.dto'
-import { UpdateFilmDto } from './dto/update-film.dto'
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard'
 import { SearchFilmDto } from './dto/searchg-film.dto'
 
@@ -15,29 +14,9 @@ export class FilmController {
     addToList(@Body() addFilmToListDto: AddFilmToListDto) {
         return this.filmService.addToList(addFilmToListDto)
     }
-    //
-    // @Get()
-    // findAll() {
-    //     return this.filmService.findAll()
-    // }
-
-    @Get(':id')
-    findOne(@Param('id') id: string) {
-        return this.filmService.findOne(+id)
-    }
 
     @Get()
     findByConditions(@Query() query: SearchFilmDto) {
         return this.filmService.findByConditions(query)
-    }
-
-    @Patch(':id')
-    update(@Param('id') id: string, @Body() updateFilmDto: UpdateFilmDto) {
-        return this.filmService.update(+id, updateFilmDto)
-    }
-
-    @Delete(':id')
-    remove(@Param('id') id: string) {
-        return this.filmService.remove(+id)
     }
 }
--- src/modules/film/film.service.ts
+++ src/modules/film/film.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@nestjs/common'
 import { AddFilmToListDto } from './dto/add-film-to-list.dto'
-import { UpdateFilmDto } from './dto/update-film.dto'
 import { Repository } from 'typeorm'
 import { InjectRepository } from '@nestjs/typeorm'
 import { FilmEntity } from './entities/film.entity'
 import ListService from '../list/list.service'
+import { SearchFilmDto } from './dto/searchg-film.dto'
 
 @Injectable()
 export class FilmService {
@@ -25,19 +25,62 @@ export class FilmService {
         return this.filmRepository.save(film)
     }
 
-    findAll() {
-        return `This action returns all film`
-    }
+    async findByConditions(dto: SearchFilmDto) {
+        // Получение ID-шников фильмов подпадающих под условия
+        const qb = this.filmRepository.createQueryBuilder('film')
+            .select(['film.id'])
+            .innerJoin('film.genres', 'genres')
+            .innerJoin('film.countries', 'countries')
+            .where(`film.release_date BETWEEN :yearFrom AND :yearTo`)
+            .andWhere(`film.vote_average BETWEEN :ratingFrom AND :ratingTo`)
+            .setParameters({
+                yearFrom: new Date(dto.yearFrom),
+                yearTo: new Date(dto.yearTo),
+                ratingFrom: dto.ratingFrom,
+                ratingTo: dto.ratingTo,
+                country: dto.country,
+                genre: dto.genre,
+            })
+            .skip(dto.skip || 0)
+            .take(dto.take || 100)
 
-    findOne(id: number) {
-        return `This action returns a #${id} film`
-    }
+        if (dto.country) {
+            qb.andWhere(`countries.id = :country`)
+        }
 
-    update(id: number, updateFilmDto: UpdateFilmDto) {
-        return `This action updates a #${id} film`
-    }
+        if (dto.genre) {
+            qb.andWhere(`genres.id = :genre`)
+        }
 
-    remove(id: number) {
-        return `This action removes a #${id} film`
+        const [filmIds, total] = await qb.getManyAndCount()
+
+
+        //Получение данных по этим фильмам
+        const items = await this.filmRepository.createQueryBuilder('film')
+            .select([
+                'film.id',
+                'film.title',
+                'film.release_date',
+                'film.poster_path',
+                'film.vote_average',
+                'film.vote_count',
+                'countries',
+                'genres'
+            ])
+            .innerJoin('film.genres', 'genres')
+            .innerJoin('film.countries', 'countries')
+            .whereInIds(filmIds)
+            .getMany()
+
+        // преобразую массив объектов стран и жанров в массивы строк с названием стран и жанров
+        const listFilms = items.map(f => ({
+            ...f,
+            release_date: f.release_date.getFullYear(),
+            countries: f.countries.map(c => c.name_ru),
+            genres: f.genres.map(g => g.name),
+
+        }))
+
+        return {listFilms, total}
     }
 }
